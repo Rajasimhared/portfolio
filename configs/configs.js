26b94e2b23723a83c1706f5aabb74815feee0b61
Rajasimha Reddy <rajasimha5599@gmail.com>
2022-09-09 12:10:55 +0530

    update resume, add typedjs

--- configs/configs.js
+++ configs/configs.js
@@ -1,45 +1,52 @@
-const projects = [{
-        id: 'tryouts',
-        name: 'Tryouts',
-        description: 'Web based chat application that has features like login, signup with profile pic, search friends, create chat, create a group chat, remove members from group chat',
-        githubLink: 'https://github.com/Rajasimhared/tryouts',
-        projectLink: 'https://tryouts-nu.vercel.app'
-    },
-    {
-        id: "memory-game",
-        name: 'Memory game',
-        description: 'Remember in our early days of mobile revolution, we had this memory game with two dimensionl grid that had multiple small boxes with images. This grid would flip after some time and we have to match the set of similar images by clicking on these boxes.<br>    I have recreated this experience for mobile and desktop just for fun. It is developed using the web components.<br>Additionally, you can try to increase the complexity by choosing your own grid size.',
-        githubLink: 'https://github.com/Rajasimhared/memory-game',
-        projectLink: 'https://dynamic-memory-game.surge.sh'
-    },
-    {
-        id: "memory-game-2",
-        name: 'Memory Game inspired by F1',
-        description: 'Inspired by the preparation games that F1 drivers play in order to improve their concentration, focus and coordination, I built this game containing one dimensional blocks. On start, player has to memorise the block blink sequence and select the sequence once the blinking stops. The score would go up and also the frequency of blinks would go up as the player succeeds.<br>We show the score and high score on the top of the screen.',
-        githubLink: 'https://github.com/Rajasimhared/memory-game-2',
-        projectLink: 'https://memory-game-clicks.surge.sh'
-    },
-    {
-        id: "online-editor",
-        name: 'Online Editor',
-        description: 'Custom documents manager which allows to create, edit documents by uploading pdf template and download the final draft in docx or pdf format.<br>This was developed for a specific requirement as part of our freelance work',
-        githubLink: 'https://github.com/Rajasimhared/online-editor',
-        projectLink: 'https://delonline-editor.herokuapp.com'
-    },
-    {
-        id: "roll-ball",
-        name: 'Unity Roll a Ball',
-        description: 'While exploring unity 3D I have build this simple game that involves different geometries, textures, laws of physics. <br>Build in the unity application was ported to web using the webGL renderer.',
-        githubLink: '',
-        projectLink: 'https://webgl-rollaball.surge.sh'
-    },
+const projects = [
+  {
+    id: "tryouts",
+    name: "Tryouts",
+    description:
+      "Web based chat application that has features like login, signup with profile pic, search friends, create chat, create a group chat, remove members from group chat",
+    githubLink: "https://github.com/Rajasimhared/tryouts",
+    projectLink: "https://tryouts-nu.vercel.app",
+  },
+  {
+    id: "memory-game",
+    name: "Memory game",
+    description:
+      "Remember in our early days of mobile revolution, we had this memory game with two dimensionl grid that had multiple small boxes with images. This grid would flip after some time and we have to match the set of similar images by clicking on these boxes.<br>    I have recreated this experience for mobile and desktop just for fun. It is developed using the web components.<br>Additionally, you can try to increase the complexity by choosing your own grid size.",
+    githubLink: "https://github.com/Rajasimhared/memory-game",
+    projectLink: "https://dynamic-memory-game.surge.sh",
+  },
+  {
+    id: "memory-game-2",
+    name: "Memory Game inspired by F1",
+    description:
+      "Inspired by the preparation games that F1 drivers play in order to improve their concentration, focus and coordination, I built this game containing one dimensional blocks. On start, player has to memorise the block blink sequence and select the sequence once the blinking stops. The score would go up and also the frequency of blinks would go up as the player succeeds.<br>We show the score and high score on the top of the screen.",
+    githubLink: "https://github.com/Rajasimhared/memory-game-2",
+    projectLink: "https://memory-game-clicks.surge.sh",
+  },
+  {
+    id: "online-editor",
+    name: "Online Editor",
+    description:
+      "Custom documents manager which allows to create, edit documents by uploading pdf template and download the final draft in docx or pdf format.<br>This was developed for a specific requirement as part of our freelance work",
+    githubLink: "https://github.com/Rajasimhared/online-editor",
+    projectLink: "https://delonline-editor.herokuapp.com",
+  },
+  {
+    id: "roll-ball",
+    name: "Unity Roll a Ball",
+    description:
+      "While exploring unity 3D I have build this simple game that involves different geometries, textures, laws of physics. <br>Build in the unity application was ported to web using the webGL renderer.",
+    githubLink: "",
+    projectLink: "https://webgl-rollaball.surge.sh",
+  },
 ];
 
-const experiences = [{
-        title: 'Senior Technical Consultant',
-        company: 'Salesforce, Inc.',
-        duration: 'Since January 2020',
-        workDescription: `
+const experiences = [
+  {
+    title: "Senior Technical Consultant",
+    company: "Salesforce, Inc.",
+    duration: "Since January 2020",
+    workDescription: `
     <ul>
         <li>
             Built multiple anonymous user flow where user can choose and
@@ -73,28 +80,28 @@ const experiences = [{
                 120 sec</strong> earlier,
             performance improved by <strong>100%</strong>
         </li>
-        <li>
-            Created custom web components from <strong>scratch</strong> which
-            were later used
-            across many projects, as there were no OOTB components for the
-            designs.
-        </li>
-        <li>
-            Active member of <strong>POC</strong> team
-        </li>
+        <li>Developed  <strong>reusable </strong> omniscripts architecture that was used across multiple projects hence reducing the development time
+to  <strong>30% </strong> and also proved  <strong>cost-effective </strong>(less than half the amount) to the customer.</li>
+        <li> Created custom web components from  <strong>scratch </strong> which were later used across many projects, as there were no OOTB
+components for the designs.</li>
+        <li>  <strong>Product Distribution </strong>: Designed and developed an architecture to  <strong>publish products </strong> from the org to third-party systems,
+this helped the customer to create a  <strong>recommendation engine </strong> that can serve users’ best offers.</li>
+        <li> Worked extensively on the  <strong>Digital Commerce APIs, Cart APIs, Product Configuration, Apex, Omniscripts, Flexcards,
+IP and DRs </strong>. Actively involved in  <strong>POCs </strong>, which helped deliver things on time.</li>
+        <li> Hold two certifications  <strong>Omnistudio Platform Developer </strong> and  <strong>Javascript Developer I </strong>. Also, I am a  <strong>Trailhead Ranger </strong>.</li>
         <li>Used <strong>Polymerjs, lit-element, web components, nodejs,
                 expressjs,
                 lwc.</strong>
         </li>
     </ul>
     `,
-        logo: 'salesforce-logo.svg'
-    },
-    {
-        title: 'Software Development Engineer',
-        company: 'Youplus, Inc.',
-        duration: 'June 2018 - December 2019',
-        workDescription: `
+    logo: "salesforce-logo.svg",
+  },
+  {
+    title: "Software Development Engineer",
+    company: "Youplus, Inc.",
+    duration: "June 2018 - December 2019",
+    workDescription: `
         <ul>
         <li>
             <strong>VIEW Configuration Portal:</strong> Youplus is the world’s
@@ -145,13 +152,13 @@ const experiences = [{
         </li>
     </ul>
     `,
-        logo: 'youplus-logo.svg'
-    },
-    {
-        title: 'Frontend Intern',
-        company: 'Scapic (Acquired by Flipkart)',
-        duration: 'January 2018 - May 2018',
-        workDescription: `
+    logo: "youplus-logo.svg",
+  },
+  {
+    title: "Frontend Intern",
+    company: "Scapic (Acquired by Flipkart)",
+    duration: "January 2018 - May 2018",
+    workDescription: `
         <ul>
         <li>
             Built <strong>Virtual Reality</strong> experiences for businesses
@@ -174,13 +181,13 @@ const experiences = [{
 
     </ul>
     `,
-        logo: 'scapic-logo.svg'
-    },
-    {
-        title: 'Summer School Intern',
-        company: 'Indian Institute of Science',
-        duration: 'July 2017',
-        workDescription: `
+    logo: "scapic-logo.svg",
+  },
+  {
+    title: "Summer School Intern",
+    company: "Indian Institute of Science",
+    duration: "July 2017",
+    workDescription: `
         <ul>
         <li>
             Chosen to be the student amongst thousands who applied for
@@ -200,11 +207,8 @@ const experiences = [{
             I also won a prize in the quiz on the final day. </li>
     </ul>
     `,
-        logo: 'iisc-logo.png'
-    }
-]
+    logo: "iisc-logo.png",
+  },
+];
 
-export {
-    projects,
-    experiences
-}
+export { projects, experiences };
--- main.js
+++ main.js
@@ -1,15 +1,30 @@
-import './style.css'
-import * as THREE from 'three';
-import {
-    GLTFLoader
-} from 'three/examples/jsm/loaders/GLTFLoader';
-import {
-    OrbitControls
-} from 'three/examples/jsm/controls/OrbitControls.js';
-import './components/AnimationControls';
-import './components/ProjectItems';
-import './components/WorkExperience';
+import "./style.css";
+import * as THREE from "three";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader";
+import { OrbitControls } from "three/examples/jsm/controls/OrbitControls.js";
+import "./components/AnimationControls";
+import "./components/ProjectItems";
+import "./components/WorkExperience";
 // Used to play/pause using controls in UI
+import Typed from "typed.js";
+
+var options = {
+  strings: [
+    "Software Developer",
+    "Web Technophile",
+    "Frontend Expert",
+    "Salesforce Enthusiast",
+  ],
+  typeSpeed: 100,
+  smartBackspace: true,
+  loop: true,
+  loopCount: Infinity,
+  showCursor: false,
+  backDelay: 700,
+  backSpeed: 50,
+};
+
+var typed = new Typed("#my-description", options);
 
 let animationId;
 let audio;
@@ -19,7 +34,12 @@ scene.background = new THREE.Color(0x91d1ff);
 
 // Camera
 
-const camera = new THREE.PerspectiveCamera(45, window.innerWidth / window.innerHeight, 0.5, 1000);
+const camera = new THREE.PerspectiveCamera(
+  45,
+  window.innerWidth / window.innerHeight,
+  0.5,
+  1000
+);
 
 var clock = new THREE.Clock();
 var angle = 0;
@@ -29,7 +49,7 @@ var radius = 80;
 
 // Light
 
-const ambientLight = new THREE.AmbientLight(0xFFFFFF);
+const ambientLight = new THREE.AmbientLight(0xffffff);
 ambientLight.intensity = 1;
 scene.add(ambientLight);
 
@@ -38,20 +58,20 @@ spotLight.position.set(10, 10, 100);
 scene.add(spotLight);
 
 const pointLight = new THREE.PointLight(0x008000);
-pointLight.position.set(5, 10, 100)
+pointLight.position.set(5, 10, 100);
 pointLight.intensity = 2;
 scene.add(pointLight);
 
-const pointLight2 = new THREE.PointLight(0xFF0000);
-pointLight2.position.set(5, -10, 100)
+const pointLight2 = new THREE.PointLight(0xff0000);
+pointLight2.position.set(5, -10, 100);
 pointLight2.intensity = 1;
 scene.add(pointLight2);
 
 // Renderer
 
 const renderer = new THREE.WebGLRenderer({
-    canvas: document.querySelector('#main-content'),
-    antialias: true
+  canvas: document.querySelector("#main-content"),
+  antialias: true,
 });
 renderer.setSize(window.innerWidth, window.innerHeight);
 renderer.setPixelRatio(window.devicePixelRatio);
@@ -69,9 +89,9 @@ controls.maxPolarAngle = Math.PI;
 
 const loader = new GLTFLoader();
 let floatingIsland;
-loadModel('mystic_stones_of_the_sky/scene.gltf').then(model => {
-    floatingIsland = model;
-    scene.add(floatingIsland);
+loadModel("mystic_stones_of_the_sky/scene.gltf").then((model) => {
+  floatingIsland = model;
+  scene.add(floatingIsland);
 });
 
 // Geometries
@@ -91,105 +111,126 @@ const sphereGeometry = new THREE.SphereGeometry(6, 32, 16);
 const torusGeometry = new THREE.TorusGeometry(4, 1, 16, 60);
 
 const createMeshRandomAndRotate = (geometry, material) => {
-    const mesh = new THREE.Mesh(geometry, material);
+  const mesh = new THREE.Mesh(geometry, material);
 
-    const [x, y, z] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(300));
-    mesh.position.set(x, y, z);
+  const [x, y, z] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(300));
+  mesh.position.set(x, y, z);
 
-    const [rx, ry, rz] = Array(3).fill().map(() => THREE.MathUtils.randFloatSpread(1));
-    mesh.rotation.set(rx, ry, rz);
+  const [rx, ry, rz] = Array(3)
+    .fill()
+    .map(() => THREE.MathUtils.randFloatSpread(1));
+  mesh.rotation.set(rx, ry, rz);
 
-    scene.add(mesh);
-}
+  scene.add(mesh);
+};
 
-Array(100).fill().map(() => createMeshRandomAndRotate(crystalGeometry, whiteBasicMaterial));
-Array(100).fill().map(() => createMeshRandomAndRotate(sphereGeometry, whiteBasicMaterial));
-Array(100).fill().map(() => createMeshRandomAndRotate(torusGeometry, whiteBasicMaterial));
+Array(100)
+  .fill()
+  .map(() => createMeshRandomAndRotate(crystalGeometry, whiteBasicMaterial));
+Array(100)
+  .fill()
+  .map(() => createMeshRandomAndRotate(sphereGeometry, whiteBasicMaterial));
+Array(100)
+  .fill()
+  .map(() => createMeshRandomAndRotate(torusGeometry, whiteBasicMaterial));
 
 //Animation
 
 const animate = () => {
-    animationId = requestAnimationFrame(animate);
-    delta = clock.getDelta();
-    camera.position.x = Math.cos(angle) * radius;
-    camera.position.z = Math.sin(angle) * radius + 40;
-    angle += angularSpeed * delta;
-    camera.lookAt(floatingIsland ? floatingIsland.position : new THREE.Vector3(0, 0, 0));
-    renderer.render(scene, camera);
-}
+  animationId = requestAnimationFrame(animate);
+  delta = clock.getDelta();
+  camera.position.x = Math.cos(angle) * radius;
+  camera.position.z = Math.sin(angle) * radius + 40;
+  angle += angularSpeed * delta;
+  camera.lookAt(
+    floatingIsland ? floatingIsland.position : new THREE.Vector3(0, 0, 0)
+  );
+  renderer.render(scene, camera);
+};
 
 animate();
 
 // Resize event listener
 
-window.addEventListener('resize', onWindowResize, true);
+window.addEventListener("resize", onWindowResize, true);
 
 function onWindowResize() {
-    camera.aspect = window.innerWidth / window.innerHeight;
-    camera.updateProjectionMatrix();
+  camera.aspect = window.innerWidth / window.innerHeight;
+  camera.updateProjectionMatrix();
 
-    renderer.setSize(window.innerWidth, window.innerHeight);
-    renderer.render(scene, camera);
+  renderer.setSize(window.innerWidth, window.innerHeight);
+  renderer.render(scene, camera);
 }
 
 // Load models generic function
 
 function loadModel(path) {
-    return new Promise((resolve, reject) => {
-        loader.load(path, function (gltf) {
-            resolve(gltf.scene);
-        }, undefined, function (error) {
-            console.error('model load error', error);
-            reject();
-        });
-    })
-
+  return new Promise((resolve, reject) => {
+    loader.load(
+      path,
+      function (gltf) {
+        resolve(gltf.scene);
+      },
+      undefined,
+      function (error) {
+        console.error("model load error", error);
+        reject();
+      }
+    );
+  });
 }
 
 // Used preloader to enable audio at the start of experience
 
-let preloader = document.querySelector('.preloader');
+let preloader = document.querySelector(".preloader");
 preloader.onclick = enterExperience;
 
-// Init function 
+// Init function
 
 function enterExperience(e) {
+  e.target.style.display = "none";
 
-    e.target.style.display = 'none'
+  audio = new Audio("./music.mp3");
+  audio.currentTime = 15;
+  audio.play();
+  audio.loop = true;
 
-    audio = new Audio('./music.mp3');
-    audio.currentTime = 15;
-    audio.play();
-    audio.loop = true;
-
-    setTimeout(() => {
-        document.getElementsByTagName('main')[0].style.display = 'block';
-        // document.getElementsByTagName('main')[0].style.width = '100%';
-        toggleAnimation();
-        loadCustomElement('c-animation-controls', null, document.querySelector('main'));
-    }, 5000);
+  setTimeout(() => {
+    document.getElementsByTagName("main")[0].style.display = "block";
+    // document.getElementsByTagName('main')[0].style.width = '100%';
+    toggleAnimation();
+    loadCustomElement(
+      "c-animation-controls",
+      null,
+      document.querySelector("main")
+    );
+  }, 5000);
 }
 
 // Toggle controls
 
-const toggleAnimation = () => animationId ? (cancelAnimationFrame(animationId),
-    animationId = null) : animate();
+const toggleAnimation = () =>
+  animationId
+    ? (cancelAnimationFrame(animationId), (animationId = null))
+    : animate();
 
-const toggleAudio = () => audio.paused ? audio.play() : audio.pause();
+const toggleAudio = () => (audio.paused ? audio.play() : audio.pause());
 
 // Used to load any defined custom html element. Takes two params name, container to query upon
 
 function loadCustomElement(name, attributes, container = document.body) {
-    const game = document.createElement(name);
-    attributes?.forEach(attribute => {
-        game.setAttribute(attribute.key, attribute.value);
-    })
-    container.appendChild(game);
+  const game = document.createElement(name);
+  attributes?.forEach((attribute) => {
+    game.setAttribute(attribute.key, attribute.value);
+  });
+  container.appendChild(game);
 }
 
 // Event Listeners
 
-document.addEventListener('animate', () => toggleAnimation());
-document.addEventListener('audio', () => toggleAudio())
+document.addEventListener("animate", () => toggleAnimation());
+document.addEventListener("audio", () => toggleAudio());
 
 // Show/Hide Projects Logic
